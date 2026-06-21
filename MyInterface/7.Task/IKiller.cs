f351bba527e677c34f75d6eb8c776fd632f89067
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-21 15:31:12 +0400

    task completed

---
+++ MyInterface/7.Task/IKiller.cs
@@ -0,0 +1,44 @@
+﻿namespace MyInterface._7.Task;
+
+interface IKiller
+{
+    void CanKill();
+}
+
+interface IFighter
+{
+    void CanAttack();
+    void CanFire();
+    void CanShot();
+}
+
+interface IManager
+{
+    void Control();
+    void CompleteMission();
+    void GatherPeople();
+}
+
+
+class Fighter : IFighter, IKiller
+{
+    public void CanAttack() => Console.WriteLine("Attack");
+    public void CanFire() => Console.WriteLine("Fire");
+    public void CanShot() => Console.WriteLine("Shot");
+
+    public void CanKill() => Console.WriteLine("Killer");
+}
+
+
+class Commander : Fighter, IManager
+{
+    public void CompleteMission() => Console.WriteLine("Complete Mission");
+    public void Control() => Console.WriteLine("Control");
+    public void GatherPeople() => Console.WriteLine("Gather People");
+}
+
+
+
+
+
+
