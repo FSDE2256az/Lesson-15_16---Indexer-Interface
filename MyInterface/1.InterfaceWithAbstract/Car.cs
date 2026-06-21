83b7f65cf2b448c648debccbb911993b31d10e1b
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-21 13:47:44 +0400

    Interface with Abstract class completed

---
+++ MyInterface/1.InterfaceWithAbstract/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace MyInterface._1.InterfaceWithAbstract;
+
+public abstract class Car { } // Signature
+
+
+// Interface
+
+// 1. Interfaceler adlandirilma zamani 'I' herfi ile baslamalidir
+// 2. Interface daxilinde yazilan methodlarin bedeni olmur ( Yeni versiyalarda mumkundur )
+// 3. Methodlar default olaraq PUBLIC olur
+
+interface ITurbo { void Turbo(); }
+interface ISport { void Sport(); }
+interface IClassic { void Classic(); }
+
+
+// Implement etmeye mecburdu Derived class
+class Mercedes : Car, ITurbo, ISport, IClassic
+{
+    public void Classic() => Console.WriteLine("Mercedes Classic");
+    public void Sport() => Console.WriteLine("Mercedes Sport");
+    public void Turbo() => Console.WriteLine("Mercedes Turbo");
+
+}
+
+class UAZ : Car, IClassic
+{
+    public void Classic() => Console.WriteLine("UAZ Classic");
+}
---
+++ MyInterface/Program.cs
@@ -0,0 +1,70 @@
+﻿using MyInterface._1.InterfaceWithAbstract;
+
+namespace MyInterface;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        #region Xulase
+
+        // Interface
+
+        // 1. Standart interface    => Dilin oz uzerinde gelenler  ( IDisposible, ICloneable )
+        // 2. Custom interface      => Bizim yaratdiqlarimiz
+
+        // /////////////////////////////// 
+
+        // Inheritance ( is a ) Pisik bir heyvar
+        // Association ( has a, part of )
+
+        // Interface ( can do ) => Multiple base 
+
+        // /////////////////////////////// 
+
+        // Interface 
+        // 1. Referance type
+        // 2. Objecti yaranmir
+        // 3. Multiple base etmek olur, yeni 1 class 1+ interface-den torene bilir
+
+        #endregion
+
+        #region InterfaceWithAbstractClass
+
+        // var mercedes = new Mercedes();
+
+        // mercedes.Turbo();
+        // mercedes.Sport();
+        // mercedes.Classic();
+
+
+        //  IClassic mercedes2 = new Mercedes();
+        //  mercedes2.Classic();
+        // 
+        //  (mercedes2 as ITurbo)!.Turbo();
+        //  (mercedes2 as ISport)!.Sport();
+
+        List<IClassic> cars = new List<IClassic>();
+
+        cars.Add(new Mercedes());
+        cars.Add(new UAZ());
+
+        foreach (var car in cars)
+        {
+            car.Classic();
+
+            if (car is ITurbo s)
+                s.Turbo();
+        
+        }
+
+
+
+        #endregion
+
+
+
+
+
+    }
+}
